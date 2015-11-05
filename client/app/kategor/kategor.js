c335f6357090c9dc9637283576e487c116094ea7
NastyaKozlova <svandnastya@mail.ru>
2015-11-05 16:36:22 +0200

    fourth commit

--- client/app/kategor/kategor.js
+++ client/app/kategor/kategor.js
@@ -10,12 +10,11 @@
         init();
 
         function init () {
-            vm.kategor = [ 	
-               { doxod: 'основная работа', rasxod: '', family: 'папа', cost: '1000', mounth: 'январь'}, 
-	           { doxod: '', rasxod: 'комунальные расходы', family: 'мама', cost: '500', mounth: ''},
-	           { doxod: 'основная работа', rasxod: '', family: 'мама', cost: '1200', mounth: 'январь'},
-	           { doxod: 'подработка', rasxod: '', family: 'папа', cost: '1200', mounth: 'январь'},
-	           { doxod: '', rasxod: 'бензин', family: 'папа', cost: '400', mounth: ''}];
+            vm.kategor = [ 	{ doxod: 'основная работа', rasxod: '', family: 'папа', cost: '1000', mounth: 'январь'}, 
+	{ doxod: '', rasxod: 'комунальные расходы', family: 'мама', cost: '500', mounth: ''},
+	{ doxod: 'основная работа', rasxod: '', family: 'мама', cost: '1200', mounth: 'январь'},
+	{ doxod: 'подработка', rasxod: '', family: 'папа', cost: '1200', mounth: 'январь'},
+	{ doxod: '', rasxod: 'бензин', family: 'папа', cost: '400', mounth: ''}];
         }
     }
 })(); 
