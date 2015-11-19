c4b2fa7d2f1d57f7e191da3a159a44e4964ba834
NastyaKozlova <svandnastya@mail.ru>
2015-11-19 12:22:27 +0200

    sixth commit

--- client/app/doxod/doxod.js
+++ client/app/doxod/doxod.js
@@ -10,10 +10,10 @@
         init();
 
         function init () {
-            vm.doxod = [ 	{ state: 'основная работа', family: 'папа', data: Date.parse('10.10.15'),  suma: '1500'}, 
-{ state: '', family: 'мама', data: Date.parse('10.10.15'),  suma: '1500'}, 
-{ state: 'подработка', family: 'папа', data: Date.parse('11.10.15'),  suma: '1800'},
-{ state: 'стипендия', family: 'дочь', data: Date.parse('12.10.15'),  suma: '1300'},  ];
+            vm.doxod = [ 	{ state: 'основная работа', family: 'папа', data:('10.10.15'),  suma: '1500'}, 
+{ state: '', family: 'мама', data: ('10.10.15'),  suma: '1500'}, 
+{ state: 'подработка', family: 'папа', data: ('11.10.15'),  suma: '1800'},
+{ state: 'стипендия', family: 'дочь', data:('12.10.15'),  suma: '1300'},  ];
         }
     }
 })(); 
